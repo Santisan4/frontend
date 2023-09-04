a8adad5adeefea4e636a047b3841662e350745de
Santiago Sanchez <sant97@hotmail.com.ar>
2023-09-03 23:41:22 -0300

    new commit

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { type User } from '../types'
 
 // const baseUrl = 'http://localhost:3000/user'
-const baseUrl = 'https://backend-hqcs-dev.fl0.io/user'
+const baseUrl = 'https://tiendaeos-dev.fl0.io/user'
 
 const login = async ({ email, password }: { email: string, password: string }): Promise<User> => {
   const { data } = await axios.post(`${baseUrl}/${'login'}`, { email, password })
